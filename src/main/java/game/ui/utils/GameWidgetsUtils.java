900e4327e60abe07948270cda61b05c6403beb05
Elizaveta Lim <eliza.volga21@gmail.com>
2025-03-28 13:25:19 +0300

    fix: removed extra color

--- src/main/java/game/ui/utils/GameWidgetsUtils.java
+++ src/main/java/game/ui/utils/GameWidgetsUtils.java
@@ -21,9 +21,7 @@ public class GameWidgetsUtils {
     }
 
     public static String colorName(Color color) {
-        if (Color.RED.equals(color)) {
-            return "Красный";
-        } else if(Color.BLUE.equals(color)) {
+        if(Color.BLUE.equals(color)) {
             return "Синий";
         }
         return "";
