a15ec534c74d71869f225da93dfeed86a770dde6
Elizaveta Lim <eliza.volga21@gmail.com>
2025-03-28 09:47:38 +0300

    feat: GameActionEvent and GameActionListener

---
+++ src/main/java/game/model/events/GameActionListener.java
@@ -0,0 +1,35 @@
+package game.model.events;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.EventListener;
+
+/**
+ * Интерфейс слушателя события класса игры {@link game.model.Game}.
+ */
+public interface GameActionListener extends EventListener {
+
+    /**
+     * Робот переместился.
+     * @param event объект события класса игры.
+     */
+    void robotIsMoved(@NotNull GameActionEvent event);
+
+    /**
+     * Робот пропустил ход.
+     * @param event объект события класса игры.
+     */
+    void robotIsSkipStep(@NotNull GameActionEvent event);
+
+    /**
+     * Робот телепортировался.
+     * @param event объект события класса игры.
+     */
+    void robotIsTeleported(@NotNull GameActionEvent event);
+
+    /**
+     * Статус игры изменился.
+     * @param event объект события класса игры.
+     */
+    void gameStatusChanged(@NotNull GameActionEvent event);
+}
