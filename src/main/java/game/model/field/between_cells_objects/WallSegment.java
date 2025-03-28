5a8a5bab3f353fdeda0ca2ad2ab3fba03306c945
Elizaveta Lim <eliza.volga21@gmail.com>
2025-03-28 09:48:52 +0300

    feat: WallSegment

---
+++ src/main/java/game/model/field/between_cells_objects/WallSegment.java
@@ -0,0 +1,24 @@
+package game.model.field.between_cells_objects;
+
+import game.model.field.BetweenCellObject;
+
+import java.util.Objects;
+
+/**
+ * Сегмент стены.
+ */
+public class WallSegment extends BetweenCellObject {
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        WallSegment wallSegment = (WallSegment) o;
+        return Objects.equals(position, wallSegment.position);
+    }
+
+    @Override
+    public String toString() {
+        return "Wall{" + "position=" + position + '}';
+    }
+}
