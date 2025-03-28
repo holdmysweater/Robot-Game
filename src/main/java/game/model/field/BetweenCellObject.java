c4ca7045fffc4e93ceda0c45e34f499accdab3d3
Elizaveta Lim <eliza.volga21@gmail.com>
2025-03-28 09:37:27 +0300

    feat: BetweenCellObject

---
+++ src/main/java/game/model/field/BetweenCellObject.java
@@ -0,0 +1,75 @@
+package game.model.field;
+
+import org.jetbrains.annotations.NotNull;
+import game.model.Direction;
+import game.model.field.between_cells_objects.BetweenCellsPosition;
+
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * Объект, располагающийся между ячейками {@link Cell}
+ */
+public abstract class BetweenCellObject {
+
+    /**
+     * Позиция объекта между ячейками
+     */
+    protected BetweenCellsPosition position;
+
+    /**
+     * Получить позицию {@link BetweenCellObject#position}.
+     * @return позиция.
+     */
+    public BetweenCellsPosition getPosition() {
+        return position;
+    }
+
+    /**
+     * Установить позицию {@link BetweenCellObject#position}.
+     * @param position позиция.
+     * @return Удалось ли установить позицию.
+     */
+    boolean setPosition(@NotNull BetweenCellsPosition position) {
+        if (this.position == null || this.position.equals(position)) {
+            this.position = position;
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    /**
+     * Может ли находиться объект в позиции.
+     * @param newPosition проверяемая позиция.
+     * @return может ли находиться объект в позиции.
+     */
+    public boolean canSetAtPosition(@NotNull BetweenCellsPosition newPosition) {
+        boolean result = true;
+        Map<Direction, Cell> neighborCells = newPosition.getNeighborCells();
+
+        var iterator = neighborCells.entrySet().iterator();
+
+        while (iterator.hasNext() && result) {
+            var i = iterator.next();
+            BetweenCellObject neighborWall = i.getValue().getNeighborBetweenCellObject(i.getKey().getOppositeDirection());
+            result = (neighborWall == null) || (neighborWall == this);
+        }
+
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BetweenCellObject that = (BetweenCellObject) o;
+        return Objects.equals(position, that.position);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(position);
+    }
+}
