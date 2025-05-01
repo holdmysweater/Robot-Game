bd3274324e2411ec8eaf38b7b25ac999af76c5dc
Ilya Kolomoitsev <i.kolomoitsev@yandex.ru>
2025-05-02 01:41:05 +0300

    fix: review Small object connections with Cell

--- src/main/java/game/model/field/core/NormalCell.java
+++ src/main/java/game/model/field/core/NormalCell.java
@@ -29,27 +29,17 @@ public class NormalCell extends AbstractCell {
      * @param smallObject объект, добавляемый в ячейку.
      */
     public boolean setSmallObject(@NotNull Battery smallObject) {
-        // Perform all my checks before initiate connection TODO translate
-
-        // Return TRUE because connection already exist with THIS small object
-        if (this.getSmallObject() == smallObject) return true;
-
-        // Return FALSE if cell can't take small object
         if (!this.canSetSmallObject()) {
             return false;
         }
 
-        // Established connection
         boolean success = smallObject.setPosition(this);
-
-        // Return FALSE if connection was failed
         if (!success) {
             return false;
         }
 
-        // Remember small object
         this.smallObject = smallObject;
-        // Connection is established correctly
+
         return true;
     }
 
