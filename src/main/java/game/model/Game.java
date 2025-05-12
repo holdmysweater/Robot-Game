9895a58051575c6ddda7d724751be99918098cfa
Ilya Kolomoitsev <i.kolomoitsev@yandex.ru>
2025-05-12 19:19:44 +0300

    feat: Game class listen GameTickGenerator and resend its tick event.
    Game class implements GameTickListener.

--- src/main/java/game/model/Game.java
+++ src/main/java/game/model/Game.java
@@ -12,11 +12,12 @@ import javax.swing.Timer;
 /**
  * Игра.
  */
-public class Game {
+public class Game implements GameTickListener {
 
-    //region КОНСТРУКТОРЫ
+//region КОНСТРУКТОРЫ
 
     public Game(Labyrinth labyrinth) {
+        this.gameTickGenerator = new GameTickGenerator(TICK_DELAY);
         start(labyrinth);
     }
 
@@ -45,6 +46,8 @@ public class Game {
         }
 
         getRobot().addRobotActionListener(new RobotObserver());
+
+        this.gameTickGenerator.start();
     }
 
     /**
@@ -96,10 +99,10 @@ public class Game {
         if (!getRobot().isCapable()) {
             if (getRobot().isTeleported()) {
                 status = GameStatus.WIN;
-            }
-            else {
+            } else {
                 status = GameStatus.LOSS;
             }
+            this.gameTickGenerator.stop();
         }
 
         setStatus(status);
@@ -167,6 +170,61 @@ public class Game {
             fireRobotIsTeleported();
         }
     }
+
+    //endregion
+
+    //region ИГРОВЫЕ ТИКИ
+
+    /**
+     * Временной интервал между тиками игры в миллисекундах.
+     */
+    private static final int TICK_DELAY = 100;
+
+    /**
+     * Генератор игровых тиков.
+     */
+    private final GameTickGenerator gameTickGenerator;
+
+
+    @Override
+    public void gameTick() {
+        this.fireNewGameTick();
+    }
+
+    //region СЛУШАТЕЛИ ТИКОВ
+    /**
+     * Список слушателей тиков игры.
+     */
+    private final ArrayList<GameTickListener> gameTickListeners = new ArrayList<>();
+
+    /**
+     * Добавить нового слушателя события тик игры.
+     *
+     * @param listener слушатель.
+     */
+    private void addTickListener(@NotNull GameTickListener listener) {
+        gameTickListeners.add(listener);
+    }
+
+    /**
+     * Удалить слушателя событий тик игры
+     *
+     * @param listener слушатель.
+     */
+    private void removeTickListener(@NotNull GameTickListener listener) {
+        gameTickListeners.remove(listener);
+    }
+
+    /**
+     * Оповестить слушателей {@link Game#gameTickListeners}, о новом тике игры.
+     */
+    private void fireNewGameTick() {
+        for (GameTickListener listener : gameTickListeners) {
+            listener.gameTick();
+        }
+    }
+
+    //endregion
 
     //endregion
 
