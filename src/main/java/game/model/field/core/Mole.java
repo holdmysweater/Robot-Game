ab77d1f7492c4c3ca2cd6e138f14f4129959184b
Ilya Kolomoitsev <i.kolomoitsev@yandex.ru>
2025-05-13 00:05:09 +0300

    feat: Created Mole class.

---
+++ src/main/java/game/model/field/core/Mole.java
@@ -0,0 +1,130 @@
+package game.model.field.core;
+
+import game.model.events.GameTickListener;
+import game.model.events.MoleActionListener;
+import game.model.field.cell_objects.NonInteractiveCellObject;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Mole implements GameTickListener {
+
+    //region КОНСТРУКТОР
+
+    /**
+     * Конструктор крота.
+     *
+     * @param field поле.
+     */
+    public Mole(Field field) {
+        this.field = field;
+    }
+
+    //endregion
+
+    //region РЫТЬ ЯМУ
+
+    /**
+     * Вероятность события "крот выроет яму когда проснётся"
+     */
+    private static int PROBABILITY = 25;
+
+    /**
+     * Поле.
+     */
+    private Field field;
+
+    /**
+     * Разбудить крота.
+     *
+     * @return Яма, если была вырыта. В противном случае null.
+     */
+    private Hole wakeUp() {
+        Random rand = new Random();
+        if (rand.nextInt(1, 101) <= PROBABILITY) {
+            return this.digHole();
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Рыть яму на поле.
+     *
+     * @return Яма, если удалось её вырыть. В противном случае null.
+     */
+    private Hole digHole() {
+        Cell cell = this.findCellForHole();
+        if (cell == null) {
+            return null;
+        }
+        Hole hole = new Hole();
+        boolean success = cell.setObject(hole);
+        assert success : "Hole must be installed.";
+        if (!success) {
+            return null;
+        }
+        this.fireMoleDigNewHole(hole);
+        return hole;
+    }
+
+
+    /**
+     * Получить свободную ячейку для новой ямы.
+     *
+     * @return Ячейка, если удалось её найти. В противном случае null.
+     */
+    private Cell findCellForHole() {
+        return this.field.getEmptyCell();
+    }
+
+    //endregion
+
+    //region СИГНАЛЫ
+
+    /**
+     * Список слушателей, подписанных на события игры.
+     */
+    private final ArrayList<MoleActionListener> moleListListener = new ArrayList<>();
+
+    /**
+     * Добавить нового слушателя за событиями игры.
+     *
+     * @param listener слушатель.
+     */
+    public void addMoleActionListener(MoleActionListener listener) {
+        moleListListener.add(listener);
+    }
+
+    /**
+     * Удалить слушателя за событиями игры.
+     *
+     * @param listener слушатель.
+     */
+    public void removeMoleActionListener(MoleActionListener listener) {
+        moleListListener.remove(listener);
+    }
+
+    /**
+     * Оповестить слушателей {@link Mole#moleListListener}, что крот вырыл новую яму.
+     *
+     * @param hole яма.
+     */
+    private void fireMoleDigNewHole(Hole hole) {
+        for (MoleActionListener listener : moleListListener) {
+            listener.digNewHole(hole);
+        }
+    }
+
+    //endregion
+
+    //region ОБРАБОТКА ТИКОВ
+
+    @Override
+    public void gameTick() {
+        wakeUp();
+    }
+
+    //endregion
+
+}
