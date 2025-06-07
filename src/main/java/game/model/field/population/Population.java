f227823ee8bd10a70ec854ccf30f0d8a2ef87311
Ilya Kolomoitsev <i.kolomoitsev@yandex.ru>
2025-06-07 22:30:58 +0300

    feat: added listeners for Population.

--- src/main/java/game/model/field/population/Population.java
+++ src/main/java/game/model/field/population/Population.java
@@ -1,9 +1,11 @@
 package game.model.field.population;
 
+import java.util.ArrayList;
+import java.util.EventListener;
 import java.util.HashSet;
 import java.util.Set;
 
-public abstract class Population<T> {
+public abstract class Population<T, K extends EventListener> {
 
     //region ОБНОВЛЕНИЕ
 
@@ -26,6 +28,33 @@ public abstract class Population<T> {
      */
     void addObject(T object) {
         objects.add(object);
+    }
+
+    //endregion
+
+    //region СЛУШАТЕЛИ
+
+    /**
+     * Список слушателей, подписанных на события популяции.
+     */
+    protected final ArrayList<K> populationListListener = new ArrayList<>();
+
+    /**
+     * Добавить нового слушателя за событиями поля.
+     *
+     * @param listener слушатель.
+     */
+    public void addPopulationActionListener(K listener) {
+        populationListListener.add(listener);
+    }
+
+    /**
+     * Удалить слушателя за событиями поля.
+     *
+     * @param listener слушатель.
+     */
+    public void removePopulationActionListener(K listener) {
+        populationListListener.remove(listener);
     }
 
     //endregion
--- src/main/java/game/model/field/population/PopulationMole.java
+++ src/main/java/game/model/field/population/PopulationMole.java
@@ -1,8 +1,11 @@
 package game.model.field.population;
 
+import game.model.events.MoleActionEvent;
+import game.model.events.MoleActionListener;
 import game.model.field.core.Mole;
+import org.jetbrains.annotations.NotNull;
 
-public class PopulationMole extends Population<Mole> {
+public class PopulationMole extends Population<Mole, MoleActionListener> {
     @Override
     void update() {
         for (Mole mole : this.objects) {
