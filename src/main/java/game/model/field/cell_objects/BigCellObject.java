4f48c8477d4b14159ceef367a442d5219a06a52e
Elizaveta Lim <eliza.volga21@gmail.com>
2025-06-09 23:34:09 +0300

    fix: BigCellObject extends StationaryCellObject

--- src/main/java/game/model/field/cell_objects/BigCellObject.java
+++ src/main/java/game/model/field/cell_objects/BigCellObject.java
@@ -2,5 +2,5 @@ package game.model.field.cell_objects;
 
 import game.model.field.core.CellObject;
 
-public abstract class BigCellObject extends CellObject {
+public abstract class BigCellObject extends StationaryCellObject {
 }
