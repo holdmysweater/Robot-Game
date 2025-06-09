e9df69ff72b5e8ec7522bad75ffc8f4ef32b85b2
Elizaveta Lim <eliza.volga21@gmail.com>
2025-06-10 00:53:14 +0300

    feat: Projectile

---
+++ src/main/java/game/model/field/cell_objects/Projectile.java
@@ -0,0 +1,42 @@
+package game.model.field.cell_objects;
+
+import game.model.field.core.Direction;
+import game.model.field.core.MobilityOnFieldProperty;
+import game.model.field.core.VisibleFieldObject;
+import org.jetbrains.annotations.NotNull;
+
+public abstract class Projectile extends VisibleFieldObject implements IMobileFieldObject, ICollidingObject {
+
+    //region ПЕРЕМЕЩЕНИЕ
+
+    /**
+     * Способность перемещаться.
+     */
+    protected final MobilityOnFieldProperty _mobility = new MobilityOnFieldProperty(this);
+
+    @Override
+    public boolean stopMoving() {
+        return _mobility.stopMoving();
+    }
+
+    @Override
+    public int getMovementSpeed() {
+        return _mobility.getMovementSpeed();
+    }
+
+    @Override
+    public Direction getMovementDirection() {
+        return _mobility.getMovementDirection();
+    }
+
+    //endregion
+
+    //region ОБЪЕКТ С КОЛЛИЗИЕЙ
+
+    @Override
+    public void processCollisionWith(@NotNull ICollidingObject object) {
+        // TODO processCollisionWith Robot
+    }
+
+    //endregion
+}
