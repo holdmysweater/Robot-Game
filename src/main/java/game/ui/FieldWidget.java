b11f511c9b9165e2600c335b63d7c20db9ca0be5
Elizaveta Lim <eliza.volga21@gmail.com>
2025-06-13 22:43:36 +0300

    fix: new FieldWidget logic

--- src/main/java/game/ui/FieldWidget.java
+++ src/main/java/game/ui/FieldWidget.java
@@ -4,6 +4,7 @@ import game.model.events.*;
 import game.model.field.core.*;
 import game.model.field.core.Point;
 import game.model.field.core.Robot;
+import game.ui.utils.ImageUtils;
 import org.jetbrains.annotations.NotNull;
 import game.ui.obstacle.BetweenCellsWidget;
 import game.ui.cell.*;
@@ -19,91 +20,55 @@ public class FieldWidget extends JLayeredPane {
 
     private final Field field;
     private final WidgetFactory widgetFactory;
-    private final JPanel gridPanel;
-
-    // Размеры клетки
-    private static final int CELL_WIDTH = Cell.DEFAULT_WIDTH;
-    private static final int CELL_HEIGHT = Cell.DEFAULT_HEIGHT;
 
     public FieldWidget(@NotNull Field field, @NotNull WidgetFactory widgetFactory) {
         this.field = field;
         this.widgetFactory = widgetFactory;
 
-        // --- Сетка ---
-        gridPanel = new JPanel();
-        gridPanel.setLayout(new BoxLayout(gridPanel, BoxLayout.Y_AXIS));
-        gridPanel.setOpaque(false);
-        fillField();
-
-        int w = field.getWidth() * CELL_WIDTH;
-        int h = field.getHeight() * CELL_HEIGHT;
-        setPreferredSize(new Dimension(w, h));
         setLayout(null);
+        setBackground(ImageUtils.BETWEEN_CELLS_COLOR);
+        setOpaque(true);
 
-        gridPanel.setBounds(0, 0, w, h);
-        add(gridPanel, JLayeredPane.DEFAULT_LAYER);
+        int cellW = Cell.DEFAULT_WIDTH;
+        int cellH = Cell.DEFAULT_HEIGHT;
+        int spacing = Field.DISTANCE_BETWEEN_CELLS;
+        int w = field.getWidth() * cellW + (field.getWidth()-1) * spacing;
+        int h = field.getHeight() * cellH + (field.getHeight()-1) * spacing;
+
+        setPreferredSize(new Dimension(w, h));
+
+        // Add all cells
+        for (int y = 0; y < field.getHeight(); ++y) {
+            for (int x = 0; x < field.getWidth(); ++x) {
+                int px = x * (cellW + spacing);
+                int py = y * (cellH + spacing);
+                Cell cell = field.getCell(new game.model.field.core.Point(x, y));
+                CellWidget cw = widgetFactory.create(cell);
+                cw.setBounds(px, py, cellW, cellH);
+                add(cw, JLayeredPane.DEFAULT_LAYER);
+
+                // Add vertical walls (between cells)
+                if (x < field.getWidth() - 1) {
+                    BetweenCellsWidget wallE = widgetFactory.create(cell.getNeighborArea(Direction.EAST));
+                    int wallW = spacing;
+                    wallE.setBounds(px + cellW, py, wallW, cellH);
+                    add(wallE, JLayeredPane.DEFAULT_LAYER);
+                }
+                // Add horizontal walls (between cells)
+                if (y < field.getHeight() - 1) {
+                    BetweenCellsWidget wallS = widgetFactory.create(cell.getNeighborArea(Direction.SOUTH));
+                    int wallH = spacing;
+                    wallS.setBounds(px, py + cellH, cellW, wallH);
+                    add(wallS, JLayeredPane.DEFAULT_LAYER);
+                }
+            }
+        }
 
         subscribeOnRobots();
         field.addFieldActionListener(new FieldController());
     }
 
-    // --- СОЗДАНИЕ ПОЛЯ ---
-    private void fillField() {
-        if (field.getHeight() > 0) {
-            JPanel startRowWalls = createRowWalls(0, Direction.NORTH);
-            gridPanel.add(startRowWalls);
-        }
-
-        for (int i = 0; i < field.getHeight(); ++i) {
-            JPanel row = createRow(i);
-            gridPanel.add(row);
-            JPanel rowWalls = createRowWalls(i, Direction.SOUTH);
-            gridPanel.add(rowWalls);
-        }
-    }
-
-    private JPanel createRow(int rowIndex) {
-        JPanel row = new JPanel();
-        row.setLayout(new BoxLayout(row, BoxLayout.X_AXIS));
-        row.setOpaque(false);
-        for (int i = 0; i < field.getWidth(); ++i) {
-            Point point = new Point(i, rowIndex);
-            Cell cell = field.getCell(point);
-            CellWidget cellWidget = widgetFactory.create(cell);
-
-            if (i == 0) {
-                BetweenCellsWidget westCellWidget = widgetFactory.create(cell.getNeighborArea(Direction.WEST));
-                row.add(westCellWidget);
-            }
-
-            row.add(cellWidget);
-
-            BetweenCellsWidget eastCellWidget = widgetFactory.create(cell.getNeighborArea(Direction.EAST));
-            row.add(eastCellWidget);
-        }
-        return row;
-    }
-
-    private JPanel createRowWalls(int rowIndex, Direction direction) {
-        if (direction == Direction.EAST || direction == Direction.WEST) throw new IllegalArgumentException();
-        JPanel row = new JPanel();
-        row.setLayout(new BoxLayout(row, BoxLayout.X_AXIS));
-        row.setOpaque(false);
-
-        for (int i = 0; i < field.getWidth(); ++i) {
-            Point point = new Point(i, rowIndex);
-            Cell cell = field.getCell(point);
-
-            BetweenCellsWidget betweenCellWidget = widgetFactory.create(cell.getNeighborArea(direction));
-            row.add(betweenCellWidget);
-        }
-        return row;
-    }
-
-    //endregion
-
-    //region СЛУШАТЕЛЬ РОБОТА
-
+    // --- ROBOT MOVEMENT ---
     private void subscribeOnRobots() {
         Robot robot = field.getRobot();
         robot.addRobotActionListener(new RobotController());
@@ -115,17 +80,9 @@ public class FieldWidget extends JLayeredPane {
      */
     private void updateRobotWidgetPosition(Robot robot, int pixelX, int pixelY) {
         RobotWidget robotWidget = (RobotWidget) widgetFactory.getWidget(robot);
-
-        // Удаляем из любого родителя
-        Container parent = robotWidget.getParent();
-        if (parent != null) parent.remove(robotWidget);
-
-        // Добавляем на слой движения, если еще не там
         if (robotWidget.getParent() != this) {
             this.add(robotWidget, JLayeredPane.DRAG_LAYER);
         }
-
-        // Центрируем по координате центра из модели
         robotWidget.setBounds(pixelX, pixelY, robotWidget.getWidth(), robotWidget.getHeight());
         robotWidget.repaint();
         this.repaint();
@@ -136,13 +93,9 @@ public class FieldWidget extends JLayeredPane {
      */
     private void snapRobotWidgetToCell(Robot robot, Cell cell) {
         RobotWidget robotWidget = (RobotWidget) widgetFactory.getWidget(robot);
-
-        // Удаляем с JLayeredPane (если был)
         if (robotWidget.getParent() == this) {
             this.remove(robotWidget);
         }
-
-        // Добавляем обратно в CellWidget
         CellWidget cellWidget = widgetFactory.getWidget(cell);
         cellWidget.addItem(robotWidget);
         cellWidget.revalidate();
@@ -168,15 +121,12 @@ public class FieldWidget extends JLayeredPane {
 
             // Центрируем по пиксельной координате из модели (минус половина размера виджета)
             int pixelX = center.getX() - widgetW / 2;
-            int pixelY = center.getY() - widgetH / 2;
+            int pixelY = center.getY() - widgetH / 2 + 5;
 
             updateRobotWidgetPosition(robot, pixelX, pixelY);
         }
     }
 
-    /**
-     * Когда робот прибыл в ячейку (вызывается только после завершения движения).
-     */
     private class RobotController implements RobotActionListener {
 
         @Override
