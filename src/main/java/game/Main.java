8e4849c4a68926b4ad5fd0a227bb926fe364bd70
Elizaveta Lim <eliza.volga21@gmail.com>
2025-06-13 23:15:47 +0300

    feat: tick delay is 1 in Game

--- src/main/java/game/Main.java
+++ src/main/java/game/Main.java
@@ -53,7 +53,7 @@ public class Main {
 
         private void startGame() {
             widgetFactory = new WidgetFactory();
-            game = new Game(new SmallLabyrinth(), 1000);
+            game = new Game(new SmallLabyrinth(), 1);
 
             game.addGameActionListener(new GameController());
 
