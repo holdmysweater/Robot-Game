c8d095eee62efcf65bf81c0347e0469c947bbca5
Ilya Kolomoitsev <i.kolomoitsev@yandex.ru>
2025-06-13 14:28:18 +0300

    feat: PopulationMobileObject monitors events of mobile objects.

--- src/main/java/game/model/field/population/PopulationMobileObject.java
+++ src/main/java/game/model/field/population/PopulationMobileObject.java
@@ -2,12 +2,45 @@ package game.model.field.population;
 
 import game.model.events.MobileObjectListener;
 import game.model.field.cell_objects.IMobileFieldObject;
+import game.model.field.core.Mole;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.EventObject;
 
 public class PopulationMobileObject extends Population<IMobileFieldObject, MobileObjectListener> {
     @Override
     void update() {
         for (IMobileFieldObject object : this.objects) {
             object.move();
+        }
+    }
+
+    @Override
+    void addObject(IMobileFieldObject object) {
+        super.addObject(object);
+        object.addMobileObjectActionListener(new PopulationMobileObject.MobileObjectObserver());
+    }
+
+    /**
+     * Класс, реализующий наблюдение за событиями {@link Mole}
+     */
+    private class MobileObjectObserver implements MobileObjectListener {
+        @Override
+        public void objectIsMoved(EventObject event) {
+            fireMobileObjectMoved(event);
+        }
+    }
+
+    /**
+     * Оповестить слушателей {@link PopulationMole#populationListListener}, что вырыта яма.
+     *
+     * @param event яма.
+     */
+    private void fireMobileObjectMoved(@NotNull EventObject event) {
+        System.out.println("Object" + event.getSource() + ": moved");
+
+        for (MobileObjectListener listener : populationListListener) {
+            listener.objectIsMoved(event);
         }
     }
 }
