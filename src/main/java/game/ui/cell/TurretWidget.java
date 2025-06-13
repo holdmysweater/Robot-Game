4bf4f8cb657f97f2af32176a0770f4a92672cb7e
Elizaveta Lim <eliza.volga21@gmail.com>
2025-06-13 23:39:15 +0300

    feat: TurretWidget

---
+++ src/main/java/game/ui/cell/TurretWidget.java
@@ -0,0 +1,34 @@
+package game.ui.cell;
+
+import game.ui.utils.ImageUtils;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class TurretWidget extends CellItemWidget {
+
+    @Override
+    protected BufferedImage getImage() {
+        BufferedImage image = null;
+        try {
+            image = ImageIO.read(new File(ImageUtils.IMAGE_PATH + "turret.png"));
+            image = ImageUtils.resizeImage(image, 120, 100);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return image;
+    }
+
+    @Override
+    public CellWidget.Layer getLayer() {
+        return CellWidget.Layer.TOP;
+    }
+
+    @Override
+    protected Dimension getDimension() {
+        return new Dimension(120, 120);
+    }
+}
