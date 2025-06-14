af45adb4e52ef272166384ef72167142f1d4d38c
Ilya Kolomoitsev <i.kolomoitsev@yandex.ru>
2025-06-14 14:07:52 +0300

    feat: Add creation of approximation rectangle to StationaryCellObject class.

--- src/main/java/game/model/field/core/StationaryCellObject.java
+++ src/main/java/game/model/field/core/StationaryCellObject.java
@@ -17,6 +17,7 @@ public abstract class StationaryCellObject extends CellObject<Cell> {
         }
 
         this.position = position;
+        createApproximatingRectangle(position);
         return true;
     }
 
