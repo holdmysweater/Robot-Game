3e563b2b3613d81a6cbfce6d272ea8d56066935f
Ilya Kolomoitsev <i.kolomoitsev@yandex.ru>
2025-06-14 14:08:19 +0300

    feat: Add population of turret.

---
+++ src/main/java/game/model/field/population/PopulationTurret.java
@@ -0,0 +1,12 @@
+package game.model.field.population;
+
+import game.model.field.core.Turret;
+
+public class PopulationTurret extends Population<Turret> {
+    @Override
+    void update() {
+        for (Turret turret : objects) {
+            turret.update();
+        }
+    }
+}
